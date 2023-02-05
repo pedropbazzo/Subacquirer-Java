def6a77c3c6147e681a29ccf8429cb96145a1d86
Pedro Paulo Bazzo <pedropbazzo@icloud.com>
2023-02-04 22:16:46 -0300

    using example in my project

--- subacquirer.sdk/src/main/java/stelo/subacquirer/sdk/Transaction.java
+++ subacquirer.sdk/src/main/java/stelo/subacquirer/sdk/Transaction.java
@@ -10,39 +10,5 @@ public class Transaction {
 		return bankSlipURL;
 	}
 
-	public Transaction(Order orderData, Payment paymentData, Customer customerData) {
-		this.orderData = orderData;
-		this.paymentData = paymentData;
-		this.customerData = customerData;
-	}
-
-	public Order getOrderData() {
-		return orderData;
-	}
-
-	public Payment getPaymentData() {
-		return paymentData;
-	}
-
-	public Customer getCustomerData() {
-		return customerData;
-	}
-
-	public Transaction setOrderData(Order orderData) {
-		this.orderData = orderData;
-
-		return this;
-	}
-
-	public Transaction setPaymentData(Payment paymentData) {
-		this.paymentData = paymentData;
-
-		return this;
-	}
-
-	public Transaction setCustomerData(Customer customerData) {
-		this.customerData = customerData;
-
-		return this;
-	}
+	// enum
 }
